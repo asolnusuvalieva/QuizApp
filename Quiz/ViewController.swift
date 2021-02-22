80822a884c38ff5639ee068b034b59e5a36c1ebc
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-02-22 19:33:05 +0600

    Add other properties for ViewController

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -5,13 +5,16 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
-    
-    
+    let questions: [String] = ["What is 7 + 7?", "What is the capital of Vermont?", "What is cognac made from?"]
+    let answers: [String] = ["14", "Montpelier", "Grapes"]
+    var currentQuestionIndex = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
     
     //MARK: - Actions
     @IBAction func showNextQuestion(){
